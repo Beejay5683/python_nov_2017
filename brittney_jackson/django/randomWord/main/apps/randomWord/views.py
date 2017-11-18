33a0741ae1f25ec93290b535a44f727aa95d7ebb
Brittney <b.nychole87@gmail.com>
2017-11-18 11:09:44 -0600

    update rando word

--- brittney_jackson/django/randomWord/main/apps/randomWord/views.py
+++ brittney_jackson/django/randomWord/main/apps/randomWord/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import render, redirect
 from django.utils.crypto import get_random_string
+import random
+import string
 
 # Create your views here.
-def rwordGen():
+def rwordGen(n):
 	return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(n))
 
 
 def index(request):
-
-	request.session['counter']=0
+	if request.session.get('counter') == None:
+		request.session['counter']=1
+		request.session['word']= rwordGen(14)
 
 	return render(request, 'randomWord/index.html')
 
 def generate(request):
 	request.session['counter'] += 1
-
-	random = rwordGen(14)# generate a random work on click of this button
+	print request.session['counter']
+	request.session['word'] = rwordGen(14)
 
  	return redirect('/')
 
 def reset(request):
-
-	request.session['counter']=0
+	request.session.clear()
 	return redirect('/')
