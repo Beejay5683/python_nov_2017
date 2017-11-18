80ca7533ef781a63d08350835260d656191d03a0
Brittney <b.nychole87@gmail.com>
2017-11-18 12:41:30 -0600

    survey form

--- brittney_jackson/django/surveyForm/surveyForm/apps/survey/views.py
+++ brittney_jackson/django/surveyForm/surveyForm/apps/survey/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
+def index(request):
+  return render(request, 'survey/index.html')
+
+def process(request):
+ 	if request.session.get('counter') == None:
+ 		request.session['counter'] = 0
+ 	
+ 	print request.session['counter']
+ 	print request.POST['name']
+ 	print request.POST['location']
+	print request.POST['language']
+	print request.POST['comment']
+
+ 	request.session['name'] = request.POST['name']
+ 	request.session['location'] = request.POST['location']
+	request.session['language'] = request.POST['language']
+	request.session['comment'] = request.POST['comment']
+
+	return redirect('/result')
+
+def result(request):
+	request.session['counter'] += 1
+	
+
+	return render(request, 'survey/result.html')
+
+
+
